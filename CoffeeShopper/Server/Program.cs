9aac45a3ad78b063f388b7900c2bd667b51c4e05
Thuan <thuantd2001hb@gmail.com>
2023-05-04 15:55:28 +0700

    blaxor setting

--- CoffeeShopper/Server/Program.cs
+++ CoffeeShopper/Server/Program.cs
@@ -8,21 +8,20 @@ if (seed)
 {
     args = args.Except(new[] { "/seed" }).ToArray();
 }
-var builder = WebApplication.CreateBuilder(args);
-var assembly = typeof(Program).Assembly.GetName().Name;
 
-var defaultConnString = builder.Configuration.GetConnectionString("DbConnection");
+var builder = WebApplication.CreateBuilder(args);
+
+var assembly = typeof(Program).Assembly.GetName().Name;
+var defaultConnString = builder.Configuration.GetConnectionString("Dbconnection");
+
 if (seed)
 {
     SeedData.EnsureSeedData(defaultConnString);
 }
 
-
-
 builder.Services.AddDbContext<AspNetIdentityDbContext>(options =>
     options.UseSqlServer(defaultConnString,
-    b=> b.MigrationsAssembly(assembly))
-    );
+        b => b.MigrationsAssembly(assembly)));
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddEntityFrameworkStores<AspNetIdentityDbContext>();
@@ -33,23 +32,24 @@ builder.Services.AddIdentityServer()
     {
         options.ConfigureDbContext = b =>
         b.UseSqlServer(defaultConnString, opt => opt.MigrationsAssembly(assembly));
-
     })
     .AddOperationalStore(options =>
     {
         options.ConfigureDbContext = b =>
-        b.UseSqlServer(defaultConnString, opt => opt.MigrationsAssembly(assembly) );
-
+        b.UseSqlServer(defaultConnString, opt => opt.MigrationsAssembly(assembly));
     })
     .AddDeveloperSigningCredential();
 
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-
 app.UseStaticFiles();
 app.UseRouting();
-
 app.UseIdentityServer();
-app.UseAuthorization();
+//app.UseAuthorization();
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapDefaultControllerRoute();
+//});
 
 app.Run();
